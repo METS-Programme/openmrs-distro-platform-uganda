9df3c15e0121d6f011448fc0900825a69985d090
Stephen S. Musoke <ssmusoke@gmail.com>
2016-06-07 07:45:47 +0300

    Replaced a hardcoded path to the home directory with the $HOME path

--- deploywar.sh
+++ deploywar.sh
@@ -5,7 +5,7 @@
 # remove the war file, exploded application in Tomcat and any custom modules uploaded
 rm -fr /usr/local/Cellar/tomcat7/7.0.64/libexec/webapps/openmrs
 rm /usr/local/Cellar/tomcat7/7.0.64/libexec/webapps/openmrs.war
-rm -fr /Users/ssmusoke/.OpenMRS/modules/*
+rm -fr $HOME/.OpenMRS/modules/*
 
 # copy the generated war file to the application folder
 cp target/openmrs.war /usr/local/Cellar/tomcat7/7.0.64/libexec/webapps
